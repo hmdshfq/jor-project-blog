678715026f1b6012c4cbc10fc60f1cae0a34df88
Hammad <hmdshfq@outlook.com>
2024-01-01 21:15:59 +0100

    feat: add division group demo component

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,30 +1,34 @@
 import React from 'react';
 import BlogHero from '@/components/BlogHero';
 import styles from './postSlug.module.css';
-import {MDXRemote} from 'next-mdx-remote/rsc';
+import { MDXRemote } from 'next-mdx-remote/rsc';
 import { loadBlogPost } from '@/helpers/file-helpers';
+import COMPONENT_MAP from '@/helpers/mdx-components';
 
-export async function generateMetadata({params}) {
-  const { frontmatter } = await loadBlogPost(params.postSlug);
-  return {
-    title: frontmatter.title,
-    description: frontmatter.abstract,
-  };
-};
+export async function generateMetadata({ params }) {
+    const { frontmatter } = await loadBlogPost(params.postSlug);
+    return {
+        title: frontmatter.title,
+        description: frontmatter.abstract,
+    };
+}
 
-async function BlogPost({params}) {
-  const { frontmatter, content } = await loadBlogPost(params.postSlug);
-  return (
-    <article className={styles.wrapper}>
-      <BlogHero
-        title={frontmatter.title}
-        publishedOn={frontmatter.publishedOn}
-      />
-      <div className={styles.page}>
-        <MDXRemote source={content} />
-      </div>
-    </article>
-  );
+async function BlogPost({ params }) {
+    const { frontmatter, content } = await loadBlogPost(params.postSlug);
+    return (
+        <article className={styles.wrapper}>
+            <BlogHero
+                title={frontmatter.title}
+                publishedOn={frontmatter.publishedOn}
+            />
+            <div className={styles.page}>
+                <MDXRemote
+                    source={content}
+                    components={COMPONENT_MAP}
+                />
+            </div>
+        </article>
+    );
 }
 
 export default BlogPost;
